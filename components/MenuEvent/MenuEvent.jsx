f0768f1392f25e17c184a551d39c9e627cb9723a
AdrienW86 <adrien_weiss@outlook.fr>
2025-05-20 10:49:16 +0200

    zoom

--- components/MenuEvent/MenuEvent.jsx
+++ components/MenuEvent/MenuEvent.jsx
@@ -1,18 +1,41 @@
+'use client';
+import { useState } from 'react';
 import Image from 'next/image';
 import styles from './menuEvent.module.css';
 
 export default function MenuEvent() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div className={styles.banner}>
-      <Image
-        src="/menu-event.png"
-        alt="Image exemple"
-        fill
-        style={{
-          objectFit: 'contain',
-          objectPosition: 'center'
-        }}
-      />
-    </div>
+    <>
+      <div className={styles.banner} onClick={() => setIsOpen(true)}>
+        <Image
+          src="/menu-event.png"
+          alt="Image exemple"
+          fill
+          style={{
+            objectFit: 'contain',
+            objectPosition: 'center',
+            cursor: 'zoom-in',
+          }}
+        />
+      </div>
+
+      {isOpen && (
+        <div className={styles.modal} onClick={() => setIsOpen(false)}>
+          <div className={styles.modalContent}>
+            <Image
+              src="/menu-event.png"
+              alt="Image agrandie"
+              fill
+              style={{
+                objectFit: 'contain',
+                objectPosition: 'center',
+              }}
+            />
+          </div>
+        </div>
+      )}
+    </>
   );
 }
