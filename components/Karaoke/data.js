bfe54aeacafd092e8821aa42f34e9fb8b216e4f0
AdrienW86 <adrien_weiss@outlook.fr>
2025-07-09 20:10:24 +0200

    mise en ligne

--- components/Karaoke/data.js
+++ components/Karaoke/data.js
@@ -2,10 +2,7 @@ const eventMenus = [
   
   {
     date: 'Vendredi 11 juillet à 20h',
-    plats: [
-      { nom: 'Galette 3 fromages', prix: '16€' },
-      { nom: 'Galette saucisse catalane', prix: '16€' },
-    ]
+    
   },
 ];
 
