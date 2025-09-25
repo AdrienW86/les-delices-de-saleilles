2d8a409045ab50c3e63df457573197a219228723
AdrienW86 <adrien_weiss@outlook.fr>
2025-09-25 19:01:19 +0200

    mise en ligne

--- components/Karaoke/data.js
+++ components/Karaoke/data.js
@@ -3,7 +3,7 @@ const eventMenus = [
   {
     date: 'Vendredi 3 octobre à 20h',
     plats: [
-      { nom: "Filet mignon de porc sauce moutarde et purée de pomme de terre maison ou filet de merlu pané sauce à l'ail et persil avec ses légumes au four. Dessert au choix : purée de mangue et sa boule de glace vanille ou tarte aux pommes", prix: "22€" },
+      { nom: "Filet mignon de porc sauce moutarde et purée de pomme de terre maison ou filet de merlu pané sauce à l'ail et persil avec ses légumes au four. Dessert au choix : purée de mangue et sa boule de glace vanille ou tarte aux pommes", prix: "23€" },
       
     ]
   },
