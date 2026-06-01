bf414329d87df6ede70ce6102cd17741f9e26b57
Codev <codev@macbookair-2.home>
2026-06-01 11:21:09 +0200

    mise en ligne

--- components/Equipment.tsx
+++ components/Equipment.tsx
@@ -51,7 +51,7 @@ export default function Equipment() {
             <span>🎉</span> Prochainement au restaurant
           </h2>
           <div className="rounded-2xl overflow-hidden shadow-md">
-            <SimpleBanner url="/mother.png" />
+            <SimpleBanner url="/dj.avif" />
           </div>
         </div>
 
